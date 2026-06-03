9340fc7f83c5ccdae0e087090b09c67217eb709b
Ohshima Yusuke <ohshima.yusuke@yukke.org>
2026-06-03 22:39:47 +0900

    ビルドスクリプトを改善

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export PROGNAME=${PROGNAME:-cdm}
+
 export GOOS=${GOOS:-linux}
 export GOARCH=${GOARCH:-amd64}
 export RELEASE_TAG=${RELEASE_TAG:-local}
 export PACKAGETYPE=${PACKAGETYPE:-tar.gz}
 
 mkdir -p dist
-binary="cdm"
+binary="${PROGNAME}"
 if [ "${GOOS}" = "windows" ]; then
     binary="${binary}.exe"
 fi
 
 go build -trimpath -ldflags="-s -w" -o "dist/${binary}" ./cmd/cdm
 
-asset_dir="cdm_${RELEASE_TAG}_${GOOS}_${GOARCH}"
+asset_dir="${PROGNAME}_${RELEASE_TAG}_${GOOS}_${GOARCH}"
 mkdir -p "dist/${asset_dir}"
 cp "dist/${binary}" "dist/${asset_dir}/"
 
@@ -24,3 +26,5 @@ else
     tar -C dist -czf "dist/${asset_dir}.tar.gz" "${asset_dir}"
 fi
 
+unset asset_dir
+unset binary
